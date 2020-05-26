1df9e5a2525f7175edf2798704c5040f18d82c97
jeo19 <terrakkk@outlook.com>
2020-05-26 22:32:39 +0800

    feat:Implement the Button toggle functionality with use the useDarkMode Hook

--- src/App.js
+++ src/App.js
@@ -1,44 +1,46 @@
-import React, { useState, useEffect } from "react";
-import { ThemeProvider } from "styled-components";
-import { GlobalStyles } from "./components/GlobalStyles";
-import { lightTheme, darkTheme } from "./components/Theme";
-import "./App.css";
-import dummyData from "./data";
-import CardList from "./components/CardList";
+import React, { useState, useEffect } from 'react';
+import { ThemeProvider } from 'styled-components';
+import { useDarkMode } from './components/useDarkMode';
+import { GlobalStyles } from './components/GlobalStyles';
+import { lightTheme, darkTheme } from './components/Theme';
+import Toggler from './components/Toggler';
+import './App.css';
+import dummyData from './data';
+import CardList from './components/CardList';
+import Toggle from './components/Toggler';
 
 const App = () => {
-  const [videos, setVideos] = useState([]);
-  const [theme, setTheme] = useState("light");
-  const themeToggler = () => {
-    theme === "light" ? setTheme("dark") : setTheme("light");
-  };
+    const [videos, setVideos] = useState([]);
+    const [theme, themeToggler] = useDarkMode();
+    const themeMode = theme === 'light' ? lightTheme : darkTheme;
+    useEffect(() => {
+        const timer = setTimeout(() => {
+            setVideos(dummyData);
+        }, 1000);
+        return () => clearTimeout(timer);
+    }, []);
 
-  useEffect(() => {
-    const timer = setTimeout(() => {
-      setVideos(dummyData);
-    }, 1000);
-    return () => clearTimeout(timer);
-  }, []);
-
-  return (
-    <ThemeProvider theme={theme === "light" ? lightTheme : darkTheme}>
-      <GlobalStyles />
-      <>
-        <div className="App">
-          <button onClick={themeToggler}>Switch Theme</button>
-          {videos.map((list, index) => {
-            return (
-              <section key={index}>
-                <h2 className="section-title">{list.section}</h2>
-                <CardList list={list} />
-                <hr />
-              </section>
-            );
-          })}
-        </div>
-      </>
-    </ThemeProvider>
-  );
+    return (
+        <ThemeProvider theme={themeMode}>
+            <GlobalStyles />
+            <>
+                <div className="App">
+                    <Toggle theme={theme} toggleTheme={themeToggler}></Toggle>
+                    {videos.map((list, index) => {
+                        return (
+                            <section key={index}>
+                                <h2 className="section-title">
+                                    {list.section}
+                                </h2>
+                                <CardList list={list} />
+                                <hr />
+                            </section>
+                        );
+                    })}
+                </div>
+            </>
+        </ThemeProvider>
+    );
 };
 
 export default App;

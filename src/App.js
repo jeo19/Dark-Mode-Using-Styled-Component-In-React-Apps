12845efe93e3ee324563717e33798f661bb0f75c
jeo19 <terrakkk@outlook.com>
2020-05-29 23:49:33 +0800

    feat:Implement Skeleton Screen UI

--- src/App.js
+++ src/App.js
@@ -3,40 +3,52 @@ import { ThemeProvider } from 'styled-components';
 import { useDarkMode } from './components/useDarkMode';
 import { GlobalStyles } from './components/GlobalStyles';
 import { lightTheme, darkTheme } from './components/Theme';
-import Toggler from './components/Toggler';
 import './App.css';
 import dummyData from './data';
 import CardList from './components/CardList';
 import Toggle from './components/Toggler';
+import SkeletonCard from './components/SkeletonCard';
 
 const App = () => {
     const [videos, setVideos] = useState([]);
     const [theme, themeToggler, mountedComponent] = useDarkMode();
+    const [loading, setLoading] = useState(false);
     const themeMode = theme === 'light' ? lightTheme : darkTheme;
     useEffect(() => {
+        setLoading(true);
         const timer = setTimeout(() => {
             setVideos(dummyData);
-        }, 1000);
+            setLoading(false);
+        }, 5000);
         return () => clearTimeout(timer);
     }, []);
+    console.log(loading);
     if (!mountedComponent) return <div />;
     return (
         <ThemeProvider theme={themeMode}>
             <GlobalStyles />
             <>
                 <div className="App">
-                    <Toggle theme={theme} toggleTheme={themeToggler}></Toggle>
-                    {videos.map((list, index) => {
-                        return (
-                            <section key={index}>
-                                <h2 className="section-title">
-                                    {list.section}
-                                </h2>
-                                <CardList list={list} />
-                                <hr />
-                            </section>
-                        );
-                    })}
+                    {loading && <SkeletonCard />}
+                    {!loading && (
+                        <Toggle
+                            theme={theme}
+                            toggleTheme={themeToggler}
+                        ></Toggle>
+                    )}
+
+                    {!loading &&
+                        videos.map((list, index) => {
+                            return (
+                                <section key={index}>
+                                    <h2 className="section-title">
+                                        {list.section}
+                                    </h2>
+                                    <CardList list={list} />
+                                    <hr />
+                                </section>
+                            );
+                        })}
                 </div>
             </>
         </ThemeProvider>
